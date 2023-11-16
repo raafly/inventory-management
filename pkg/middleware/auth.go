13a1f69bf6b75aaba36471c3d019237ccf710b07
raafly <rafliexecutor375@gmail.com>
2023-11-17 06:37:47 +0700

    [auth]: modify auth fur login

--- pkg/middleware/auth.go
+++ pkg/middleware/auth.go
@@ -7,5 +7,18 @@ type AuthMiddleware struct {
 }
 
 func (m *AuthMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-  r.Get.Cookie("auth")
+  err := r.Cookie("auth")
+  if err != nil {
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusUnauthorized)
+
+      webResponse := web.WebResponse {
+        Code: http.StatusUnauthorized,
+        Status: "UNAUTHORIZED",
+      }
+
+      helper.WriteToRequestBody(w, webResponse)
+  } else {
+    middleware.Handler.ServeHTTP(w, r)
+  }
 }
